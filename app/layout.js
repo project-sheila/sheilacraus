e77a6294ea1c23ebb67d806fee7ef79925679ac8
myt-sheila <sheila@mytdevcorp.com>
2023-09-04 22:59:02 +0800

    tooltip on navbar buttons

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,7 @@
 
 import './globals.css'
 import { Inter } from 'next/font/google'
-import { Button, Card, CardBody, Container } from '@chakra-ui/react';
+import { Button, Card, CardBody, Container, Tooltip } from '@chakra-ui/react';
 import Link from 'next/link';
 import { Providers } from "./providers";
 import { useState } from 'react';
@@ -26,18 +26,26 @@ export default function RootLayout({ children }) {
             <nav className='sticky top-0 z-1'>
               <Card className='shadow-lg rounded-lg mt-10'>
                 <CardBody className='p-3'>
+                  <Tooltip label='Home'>
                   <Link href="/">
                     <Button isActive={activeTab === "home"} onClick={()=>setActiveTab("home")}><i class="text-xl text-gray-500 fi fi-rr-home"></i></Button>
                   </Link>
+                  </Tooltip>
+                  <Tooltip label='Experiences'>
                   <Link href="/experiences">
                     <Button isActive={activeTab === "experiences"} onClick={()=>setActiveTab("experiences")}><i class="text-xl text-gray-500 fi fi-rr-briefcase"></i></Button>
                   </Link>
+                  </Tooltip>
+                  <Tooltip label='Projects'>
                   <Link href="/projects">
                     <Button isActive={activeTab === "projects"} onClick={()=>setActiveTab("projects")}><i class="text-xl text-gray-500 fi fi-rr-folder-open"></i></Button>
                   </Link>
+                  </Tooltip>
+                  <Tooltip label='Contact'>
                   <Link href="/contact">
                     <Button isActive={activeTab === "contact"} onClick={()=>setActiveTab("contact")}><i class="text-xl text-gray-500 fi fi-rr-comment-alt-dots"></i></Button>
                   </Link>
+                  </Tooltip>
                   
                 </CardBody>
               </Card>
