1b0721109d34e2f1eddaa789d3603f57c43d1720
Sheila Mae Craus <68653710+sheicraus@users.noreply.github.com>
2023-09-12 14:25:34 +0000

    updated navbar

--- app/layout.js
+++ app/layout.js
@@ -5,44 +5,59 @@ import { Inter } from 'next/font/google'
 import { Button, Card, CardBody, Container, Tooltip } from '@chakra-ui/react';
 import Link from 'next/link';
 import { Providers } from "./providers";
-import useActiveNavBar from '@/utils/useActiveNavbar';
 import Footer from '@/components/Footer';
+import { usePathname } from "next/navigation";
 
 const inter = Inter({ subsets: ['latin'] })
 
 
 export default function RootLayout({ children }) {
-  const [activeTab, setActiveNavItem] = useActiveNavBar();
+  let pathname = usePathname();
 
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body style={{backgroundColor: '#EBEFF3'}} className={inter.className}>
         <Providers>
           <Container>
 
             <nav className='sticky top-0 z-1'>
-              <Card className='shadow-lg rounded-lg mt-10'>
+              <Card className='shadow-lg rounded-lg mt-10 pt-1 pb-1'>
                 <CardBody className='p-3'>
                   <Tooltip label='Home' className='text-xs rounded-md'>
-                  <Link href="/">
-                    <Button isActive={activeTab === "home"} onClick={()=>setActiveNavItem("home")}><i class="text-xl text-gray-500 fi fi-rr-home"></i></Button>
-                  </Link>
+                    <Link href="/" className={`${(pathname === "/home" || pathname === "/") && `bg-slate-200 `} px-4 py-3 rounded-md m-1`}>
+                      <i className={`align-middle text-xl ${pathname === "/home" || pathname === "/" ? `text-slate-600` : `text-slate-300`} fi fi-rr-home`}></i>
+                    </Link>
                   </Tooltip>
                   <Tooltip label='Experiences' className='text-xs rounded-md'>
+                    <Link href="/experiences" className={`${(pathname === "/experiences") && `bg-slate-200 `} px-4 py-3 rounded-md m-1`}>
+                      <i className={`align-middle text-xl ${pathname === "/experiences" ? `text-slate-600` : `text-slate-300`} fi fi-rr-briefcase`}></i>
+                    </Link>
+                  </Tooltip>
+                  <Tooltip label='Projects' className='text-xs rounded-md'>
+                    <Link href="/projects" className={`${(pathname === "/projects") && `bg-slate-200 `} px-4 py-3 rounded-md m-1`}>
+                      <i className={`align-middle text-xl ${pathname === "/projects" ? `text-slate-600` : `text-slate-300`} fi fi-rr-folder-open`}></i>
+                    </Link>
+                  </Tooltip>
+                  <Tooltip label='Contact' className='text-xs rounded-md'>
+                    <Link href="/contact" className={`${(pathname === "/contact") && `bg-slate-200 `} px-4 py-3 rounded-md m-1`}>
+                      <i className={`align-middle text-xl ${pathname === "/contact" ? `text-slate-600` : `text-slate-300`} fi fi-rr-comment-alt-dots`}></i>
+                    </Link>
+                  </Tooltip>
+                  {/* <Tooltip label='Experiences' className='text-xs rounded-md'>
                   <Link href="/experiences">
-                    <Button isActive={activeTab === "experiences"} onClick={()=>setActiveNavItem("experiences")}><i class="text-xl text-gray-500 fi fi-rr-briefcase"></i></Button>
+                    <Button isActive={pathname === "experiences"} onClick={()=>setActiveNavItem("experiences")}><i class="text-xl text-gray-500 fi fi-rr-briefcase"></i></Button>
                   </Link>
                   </Tooltip>
                   <Tooltip label='Projects' className='text-xs rounded-md'>
                   <Link href="/projects">
-                    <Button isActive={activeTab === "projects"} onClick={()=>setActiveNavItem("projects")}><i class="text-xl text-gray-500 fi fi-rr-folder-open"></i></Button>
+                    <Button isActive={pathname === "projects"} onClick={()=>setActiveNavItem("projects")}><i class="text-xl text-gray-500 fi fi-rr-folder-open"></i></Button>
                   </Link>
                   </Tooltip>
                   <Tooltip label='Contact' className='text-xs rounded-md'>
                   <Link href="/contact">
-                    <Button isActive={activeTab === "contact"} onClick={()=>setActiveNavItem("contact")}><i class="text-xl text-gray-500 fi fi-rr-comment-alt-dots"></i></Button>
+                    <Button isActive={pathname === "contact"} onClick={()=>setActiveNavItem("contact")}><i class="text-xl text-gray-500 fi fi-rr-comment-alt-dots"></i></Button>
                   </Link>
-                  </Tooltip>
+                  </Tooltip> */}
                   
                 </CardBody>
               </Card>
